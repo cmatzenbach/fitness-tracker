9a5c820e83cf29936e72b0df975fd45624e4b042
cmatzenbach <cmatzenbach@gmail.com>
2021-02-11 01:32:18 -0600

    Added MUI theme to login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,25 @@
 import React, { FunctionComponent } from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
 import './App.css';
 import SignIn from './pages/SignIn';
+
+import { ThemeProvider } from '@material-ui/core/styles';
+import theme from './config/theme.config';
 
 const App: FunctionComponent = () => {
   return (
     <Router>
-      <Switch>
-        <Route exact path="/">
-          <SignIn />
-        </Route>
-        <Route path="/sign-up">
-          Sign Up
-        </Route>
-      </Switch>
+      <ThemeProvider theme={theme}>
+        <Switch>
+          <Route exact path="/">
+            <SignIn />
+          </Route>
+          <Route path="/sign-up">
+            Sign Up
+          </Route>
+        </Switch>
+      </ThemeProvider>
     </Router>
   );
 }
